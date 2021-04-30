ce1ee5f349080667d9ecf56703378efec0c56431
Aeneas Reina <aeneasreina@gmail.com>
2021-05-01 01:19:53 +0200

    Initial commit
    Created basic structure through Interface differentiation implementation

---
+++ AlgoDatProject/AlgoDatProject/DataStructures/Interface/IDictionary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoDatProject
+{
+    interface IDictionary
+    {
+        bool Search(int key);
+        void Insert(int key);
+        void Delete(int key);
+        void Print();
+    }
+}
